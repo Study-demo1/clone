c06c710ae3196cd58bbdc1811f91cbdc582c10ee
Study-demo1 <webramankashyap1999@gmail.com>
2023-12-27 00:18:06 -0800

    Add Files

---
+++ script.js
@@ -0,0 +1,9 @@
+let boxes = document.querySelector(".box");
+let resetbtn = document.querySelector(".reset-btn");
+
+let turnO =true;
+
+
+let arr = ["apple", "banana", "mango"];
+
+let arr2 = [["apple", "banana"], ["peas", "patato"], ["byuhu", "eghghrh"]];
